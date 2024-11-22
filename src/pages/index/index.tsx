b539da0e0d60ccebd29c22c476f5f4509988d44e
lidonglin.dl__dcar <lidonglin.dl@dcarlife.com>
2024-11-22 16:23:52 +0800

    feat: video bug

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from '@tarojs/components'
+import { View, Text, Video } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.less'
 
@@ -11,6 +11,7 @@ export default function Index () {
     <View className='index'>
       <Text>Hello world!</Text>
       <View dangerouslySetInnerHTML={{__html: '123'}} />
+      <Video src='https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4' controls={false} />
     </View>
   )
 }
